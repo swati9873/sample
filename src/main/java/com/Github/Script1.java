73d1747357a0b5739e04e366b3feab1a4d1956d4
akkis <akkis@Priya>
2022-08-24 16:16:37 +0530

    hi

--- src/main/java/com/Github/Script1.java
+++ src/main/java/com/Github/Script1.java
@@ -3,6 +3,7 @@ package com.Github;
 public class Script1 {
 	public static void main(String[] args) {
 		System.out.println("This is my sample project");
+		System.out.println("This is my sample project");
 		
 	}
 
