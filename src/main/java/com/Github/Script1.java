df4f04d8ed187554d049801aa0e19ac7f8083fe6
Dell <Dell@DESKTOP-BOQM7D0>
2022-08-24 15:23:45 +0530

    updated

--- src/main/java/com/Github/Script1.java
+++ src/main/java/com/Github/Script1.java
@@ -3,7 +3,6 @@ package com.Github;
 public class Script1 {
 	public static void main(String[] args) {
 		System.out.println("This is my sample project");
-		
+		System.out.println("This is Praveen, done with my assigned work");
 	}
-
 }
