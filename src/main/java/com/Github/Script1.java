12e3a9787ca02632604d59ae04cbccc777a4f93e
akkis <akkis@Priya>
2022-08-29 11:42:40 +0530

    hai jenkins

--- src/main/java/com/Github/Script1.java
+++ src/main/java/com/Github/Script1.java
@@ -5,6 +5,7 @@ public class Script1 {
 	public static void main(String[] args) {
 		System.out.println("This is my sample project");
 		System.out.println("project name is Techmax");
+		System.out.println("project is going to check in jenkins");
 		
 	}
 
