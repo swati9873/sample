dc0dd893a50565ff0f9a793ad56f0e2aa4efd429
Umamani <Umamani@DESKTOP-EM3OVKO>
2022-08-29 22:19:44 +0530

    Branch

--- src/main/java/com/Github/Script1.java
+++ src/main/java/com/Github/Script1.java
@@ -1,7 +1,11 @@
 package com.Github;
 
+import org.testng.annotations.Test;
+
 public class Script1 {
-	public static void main(String[] args) {
+	@Test
+	public void Sample1()
+	{
 		System.out.println("This is my sample project");
 		System.out.println("project name is Techmax");
 		
--- src/main/java/com/Github/Scripts2.java
+++ src/main/java/com/Github/Scripts2.java
@@ -1,8 +1,12 @@
 package com.Github;
 
+import org.testng.annotations.Test;
+
 public class Scripts2 {
 
-	public static void main(String[] args) {
+@Test
+public void Sample2()
+	{
 		System.out.println("welcome");
 	}
 
